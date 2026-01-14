aae508fb4894b92601ce26e6fa11b9cbce0d590f
NaXeMate <181981244+NaXeMate@users.noreply.github.com>
2026-01-14 13:52:16 +0100

    feat: initialize project with Vite, React and SpringBoot

---
+++ backend/src/main/java/com/edu/mqt/pixelarium/PixelariumApplication.java
@@ -0,0 +1,13 @@
+package com.edu.mqt.pixelarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelariumApplication.class, args);
+	}
+
+}
