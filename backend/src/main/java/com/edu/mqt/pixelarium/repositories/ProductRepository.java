489ace7b1d0bb76853fc7abe2489c83ee709a595
NaXeMate <181981244+NaXeMate@users.noreply.github.com>
2026-01-21 13:33:32 +0100

    feat: Implemented new methods in ProductRepository

--- backend/src/main/java/com/edu/mqt/pixelarium/repositories/ProductRepository.java
+++ backend/src/main/java/com/edu/mqt/pixelarium/repositories/ProductRepository.java
@@ -1,17 +1,21 @@
 package com.edu.mqt.pixelarium.repositories;
 
 import java.util.List;
-import java.util.Locale.Category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edu.mqt.pixelarium.model.Product;
+import com.edu.mqt.pixelarium.model.enumerated.Category;
+
 import java.math.BigDecimal;
 
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByNameContainingIgnoreCase(String namePart);
     List<Product> findByPrice(BigDecimal price);
+    List<Product> findByPriceBetween(BigDecimal min, BigDecimal max);
     List<Product> findBySalePrice(BigDecimal salePrice);
+    List<Product> findBySalePriceIsNotNull();
     List<Product> findByCategory(Category category);
-
+    List<Product> findByCategoryAndPrice(Category category, BigDecimal price);
+    List<Product> findByCategoryAndPriceBetween(Category category, BigDecimal min, BigDecimal max);
 }
