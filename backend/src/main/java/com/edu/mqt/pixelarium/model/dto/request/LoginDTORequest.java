99c959cf2b57c14ae5190fde5facefa0147ed918
NaXeMate <181981244+NaXeMate@users.noreply.github.com>
2026-02-17 19:39:34 +0100

    Frontend Global State Management (#7)
    * feat: Added simple login endpoint
    
    * feat: Added AuthContext and integrated with App
    
    * feat: Added CartContext with its new types and integrated with App
    
    * feat: Added CartContext with its new types and integrated with App

---
+++ backend/src/main/java/com/edu/mqt/pixelarium/model/dto/request/LoginDTORequest.java
@@ -0,0 +1,9 @@
+package com.edu.mqt.pixelarium.model.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginDTORequest(
+        @NotBlank(message = "Email is required") @Email(message = "Email must be valid") String email,
+        @NotBlank(message = "Password is required") String password) {
+}
